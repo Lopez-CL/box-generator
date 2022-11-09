80ffd714a2b5ce63d6901a5ce359743d1f5cf15e
Lopez-CL <cllopez511@gmail.com>
2022-11-08 21:44:19 -0500

    cleaing leftover content from merge

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import BlockInput from './components/BlockInput';
 import BlockDisplay from './components/BlockDisplay';
 
 function App() {
-<<<<<<< HEAD
-  const [colors, setColors] = useState([]);
-  // const addStyle = (inputColor) => {(setColors(inputColor))};
-  return (
-    <div className="App">
-      <BlockInput setColors = {setColors} colors = {colors}/> 
-      <BlockDisplay colors = {colors}/>
-=======
   const [style, setStyle] = useState([]);
   return (
     <div className="App">
       <BlockInput setStyle = {setStyle} style = {style}/> 
       <BlockDisplay style = {style}/>
->>>>>>> object-exploration
     </div>
   );
 }
--- src/components/BlockInput.jsx
+++ src/components/BlockInput.jsx
@@ -2,14 +2,6 @@ import React from 'react'
 import {useState} from 'react';
 const BlockInput = (props) => {
     const [styleColor, setStyleColor] = useState('');
-<<<<<<< HEAD
-    const {colors,setColors} = props
-    // const [arr, setArr] = useState();
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('button pressed!')
-        setColors([...colors,styleColor])
-=======
     const [styleHeight, setStyleHeight] = useState('');
     const [styleWidth, setStyleWidth] = useState('');
     // const [arr, setArr] = useState();
@@ -23,7 +15,6 @@ const BlockInput = (props) => {
         }
         console.log(holderObject);
         props.setStyle([...props.style, holderObject]);
->>>>>>> object-exploration
         setStyleColor('');
         setStyleHeight('');
         setStyleWidth('');
